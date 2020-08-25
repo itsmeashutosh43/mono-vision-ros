27ab8d10c37fd387343ec07e9af828df096b533a
itsmeashutosh43 <chapagainashutosh8@gmail.com>
2020-08-25 14:20:44 +0545

    CV bridge added

--- include/mono_vision.h
+++ include/mono_vision.h
@@ -1,77 +1,16 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/video/tracking.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+#include<ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 
 
-#include <iostream>
-#include <ctype.h>
-#include <algorithm>
-#include <iterator>
-#include <vector>
-#include <ctime>
-#include <sstream>
-#include <fstream>
-#include <string>
-#include <limits.h>
-#include <unistd.h>
+class MonoVision{
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub;
 
-
-using namespace cv;
-using namespace std;
-
-
-
-std::string getexepath()
-{
-  char result[ PATH_MAX ];
-  ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
-  return std::string( result -8 , (count > 1) ? count : 0 );
-}
-
-
-void featureDetection(cv::Mat &img,std::vector<cv::Point2f>& points)
-{
-    vector<cv::KeyPoint> key_point1;
-    int fast_threshold = 20;
-    bool nonMaxSupress = true;
-    cv::FAST(img,key_point1,fast_threshold,nonMaxSupress);
-
-    KeyPoint::convert(key_point1, points);
-
-}
-
-
-void featureTracking(cv::Mat& img1, cv::Mat& img2,std::vector<cv::Point2f>& points1 , std::vector<cv::Point2f>& points2,std::vector<uchar>& status)
-{
-    std::vector<float> err;
-    cv::Size winSize = cv::Size(21,21);
-
-    cv::TermCriteria termCrit = cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS,30,0.1);
-
-    cv::calcOpticalFlowPyrLK(img1,img2,points1,points2,status,err,winSize,3,termCrit,0,0.001);
-
-
-    int indexCorrection = 0;
-
-    for(int i = 0; i< status.size() ; i++)
-    {
-        cv::Point2f pt = points2.at(i - indexCorrection);
-
-        if ((status.at(i) == 0) || (pt.x < 0)  || (pt.y <0))
-        {
-            if ((pt.x < 0) ||(pt.y < 0))
-            {
-                status.at(i) = 0;
-            }
-            points1.erase(points1.begin() + i - indexCorrection);
-            points2.erase(points2.begin() + i - indexCorrection);
-
-            indexCorrection++;
-        }
-    } 
-}
-
+    public:
+    MonoVision();
+    ~MonoVision();
+    void imageCb(const sensor_msgs::ImageConstPtr& msg); 
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include "mono_vision.h"
+#include "utils.h"
 
-int main()
+MonoVision::MonoVision():it_(nh_)
 {
-    return 1;
+    image_sub = it_.subscribe("/mybot/camera/image_raw", 1 , &MonoVision::imageCb, this);
+    cv::namedWindow("Image from camera"); 
+}
+
+MonoVision::~MonoVision()
+{
+    cv::destroyWindow("Image from camera");
+}
+
+void MonoVision::imageCb(const sensor_msgs::ImageConstPtr& msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+
+    try{
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+    }
+    catch(cv_bridge::Exception& e)
+    {
+        ROS_ERROR("CV bridge exception %s",e.what());
+        return;
+    }
+
+    cv::imshow("Image from camera", cv_ptr->image);
+    cv::waitKey(3);
+
+
+}
+
+
+int main(int argc , char** argv)
+{
+    ros::init(argc, argv ,"Mono_vision");
+    MonoVision mv;
+    ros::spin();
+    return 0;
+    
 }
